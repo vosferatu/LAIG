22dd2d59bd629ec117efac611cbae21f765e8c3d
JMendes25 <up201505439@fe.up.pt>
2017-11-07 13:08:15 +0000

    end class

--- project/MyLinearAnimation.js
+++ project/MyLinearAnimation.js
@@ -7,6 +7,8 @@
 function MyLinearAnimation(scene, args) {
   CGFobject.call(this,scene);
 
+  this.speed = 10;
+
   this.animation = null;//stores linear or circular or bezier or combo animations
 };
 
